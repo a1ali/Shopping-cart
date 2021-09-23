5a98da4688bffc253c59c24f2e7a3892c75b7b34
Afaq Ali <afaqali2010@gmail.com>
2021-09-23 01:40:16 -0400

    fixed subtotal bug

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -9,23 +9,29 @@ const Cart = () => {
     let [subTotal, setSubtotal] = useState(0);
 
     const updateTotal = () => {
-        cart.forEach(item => {
-            setSubtotal((subTotal + (item.price * item.quantity)).toFixed(2))
+        let total = 0;
+        cart.forEach((item) => {
+            total += item.price * item.quantity
         });
-    }
+        setSubtotal(total.toFixed(2))
+  
+    };
 
     useEffect(() => {
-        updateTotal()
-    }, [])
+        updateTotal();
+    }, [cart]);
+
     return (
         <div className="font-roboto">
-            <div className="flex items-center justify-between mx-4 pt-4 my-2">
+            <div className="flex items-center justify-between mx-4 md:mx-20 pt-4 my-2">
                 <h1 className="text-3xl  md:text-5xl font-semibold md:my-8 text-gray-700">
                     My Cart
                 </h1>
                 <div className="bg-gray-900 flex text-gray-100 w-44 px-2 py-2 text-md divide-x-2 divide-opacity-30 divide-gray-200 rounded-sm">
                     <span className="px-2">Subtotal</span>
-                    <span className="px-2 mr-2 w-28 flex items-center justify-center">${subTotal}</span>
+                    <span className="px-2 mr-2 w-28 flex items-center justify-center">
+                        ${subTotal}
+                    </span>
                 </div>
             </div>
             {cart.length === 0 ? (
@@ -39,7 +45,7 @@ const Cart = () => {
                 </div>
             ) : (
                 cart.map((item) => (
-                    <div className="relative mx-4 my-6 md:mx-20 flex items-center border p-2 divide-x-2 divide-gray-300 divide-opacity-50 rounded-sm  hover:shadow-md border-opacity-50 transform duration-200 ease-in">
+                    <div key={item.id} className="relative mx-4 my-6 md:mx-20 flex items-center border p-2 divide-x-2 divide-gray-300 divide-opacity-50 rounded-sm  hover:shadow-md border-opacity-50 transform duration-200 ease-in">
                         <Link
                             className="mr-3 h-20 w-20 object-contain flex items-center justify-center"
                             to={`/Shopping-cart/shop/${item.itemId}`}
@@ -61,15 +67,11 @@ const Cart = () => {
                             <h1>Size: {item.size}</h1>
                             <h1>Quantity: {item.quantity}</h1>
                             <h1>
-                                Price: $
+                                Total Price: $
                                 {(item.quantity * item.price).toFixed(2)}
-                                {/* {updateTotal(item.price, item.quantity)} */}
-                                 
                             </h1>
                         </div>
                     </div>
-                   
-                    
                 ))
             )}
         </div>
