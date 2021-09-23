7b37584ffcb8180e0a797a79825f2bebf6b5433c
Afaq Ali <afaqali2010@gmail.com>
2021-09-23 01:17:25 -0400

    display subtotal on cart page

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { bindActionCreators } from "redux";
 import { actionCreators } from "../state/allActions";
@@ -6,12 +6,28 @@ import { Link } from "react-router-dom";
 
 const Cart = () => {
     let cart = useSelector((state) => state.cartReducer);
+    let [subTotal, setSubtotal] = useState(0);
 
+    const updateTotal = () => {
+        cart.forEach(item => {
+            setSubtotal((subTotal + (item.price * item.quantity)).toFixed(2))
+        });
+    }
+
+    useEffect(() => {
+        updateTotal()
+    }, [])
     return (
         <div className="font-roboto">
-            <h1 className="text-3xl text-center md:text-5xl font-semibold mx-4 my-2 md:my-8 text-gray-700">
-                My Cart
-            </h1>
+            <div className="flex items-center justify-between mx-4 pt-4 my-2">
+                <h1 className="text-3xl  md:text-5xl font-semibold md:my-8 text-gray-700">
+                    My Cart
+                </h1>
+                <div className="bg-gray-900 flex text-gray-100 w-44 px-2 py-2 text-md divide-x-2 divide-opacity-30 divide-gray-200 rounded-sm">
+                    <span className="px-2">Subtotal</span>
+                    <span className="px-2 mr-2 w-28 flex items-center justify-center">${subTotal}</span>
+                </div>
+            </div>
             {cart.length === 0 ? (
                 <div className="flex h-70v flex-col items-center justify-center">
                     <span className="text-7xl md:text-9xl my-4 md:my-6">
@@ -44,12 +60,16 @@ const Cart = () => {
                             </div>
                             <h1>Size: {item.size}</h1>
                             <h1>Quantity: {item.quantity}</h1>
-                            <h1>Price: {item.quantity * item.price}</h1>
-                        </div>
-                        <div className="absolute top-0 right-0">
-                            {/* <span className="text-4xl font-light transform rotate-45 cursor-pointer">+</span> */}
+                            <h1>
+                                Price: $
+                                {(item.quantity * item.price).toFixed(2)}
+                                {/* {updateTotal(item.price, item.quantity)} */}
+                                 
+                            </h1>
                         </div>
                     </div>
+                   
+                    
                 ))
             )}
         </div>
