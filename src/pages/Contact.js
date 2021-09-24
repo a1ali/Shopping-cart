712601da01934abd0dc558fcd3ca24311b10cb6f
Afaq Ali <afaqali2010@gmail.com>
2021-09-24 15:39:15 -0400

    added contact info with simple animation

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
 
 const Contact = () => {
     return (
-        <div>
-            Hello from Contact
-        </div>
-    )
-}
+        <div className="bg-hero-pattern h-93v 2xl:h-95v flex items-center justify-center font-roboto">
+            <motion.div
+                className="w-2/3 h-1/3 bg-gray-100 shadow-md rounded-sm flex flex-col items-center py-8 px-4"
+                variants={{
+                    hidden: {
+                        opacity: 0,
+                        scale: 0,
+                    },
+                    visible: {
+                        opacity: 1,
+                        scale: 1,
+                        transition: {
+                            duration: 0.25,
+                            ease:"easeIn"
+                        }
+                    },
+                }}
+                initial="hidden"
+                animate="visible"
+            >
+                <span className="text-xl tracking-tight font-semibold text-gray-700 sm:text-3xl md:text-4xl 2xl:text-6xl mb-4">
+                    ALLMART Contact Info
+                </span>
 
-export default Contact
+                <div className="flex flex-col items-center justify-center text-md sm:text-lg md:text-xl md:mt-2 lg:mt-16">
+                    <span>Customer Service Hours:</span>
+                    <span>Monday-Friday</span>
+                    <span>9AM - 6PM EST</span>
+                    <span>Email:</span>
+                    <span>customerservice@allmart.com</span>
+                </div>
+            </motion.div>
+        </div>
+    );
+};
+
+export default Contact;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { bindActionCreators } from "redux";
 import { actionCreators } from "../state/allActions";
+import { motion } from "framer-motion";
 
 const Home = () => {
     const dispatch = useDispatch();
@@ -11,13 +12,29 @@ const Home = () => {
 
     return (
         <main class="h-93v 3xl:h-95v flex flex-col md:flex-row items-center w-screen">
-            <div class="px-4 sm:px-6 lg:px-8 2xl:px-16 pt-10 sm:pt-12 lg:pt-20 xl:pt-28 pb-4 sm:text-center lg:text-left w-full md:w-1/2 h-1/2 md:h-full flex justify-center flex-col bg-hero-pattern">
-                <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl 2xl:text-8xl">
+            <motion.div class="px-4 sm:px-6 lg:px-8 2xl:px-16 pt-10 sm:pt-12 lg:pt-20 xl:pt-28 pb-4 sm:text-center lg:text-left w-full md:w-1/2 h-1/2 md:h-full flex justify-center flex-col bg-hero-pattern">
+                <motion.h1
+                    class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl 2xl:text-8xl"
+                    variants={{
+                        hidden: {
+                            opacity: 0,
+                            y: "-30px",
+                        },
+                        visible: {
+                            opacity: 1,
+                            y: "0px",
+                            transition: {
+                                delay: 0.03,
+                                ease: "easeIn"
+                            }
+                        },
+                    }}
+                    initial="hidden"
+                    animate="visible"
+                >
                     <span class="block">The shop for all</span>
-                    <span class="block text-indigo-600">
-                    your buying needs
-                    </span>
-                </h1>
+                    <span class="block text-indigo-600">your buying needs</span>
+                </motion.h1>
                 <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
                     Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
                     qui lorem cupidatat commodo. Elit sunt amet fugiat veniam
@@ -43,15 +60,13 @@ const Home = () => {
                         </Link>
                     </div>
                 </div>
-            </div>
+            </motion.div>
 
-            
-                <img
-                    className="w-full md:w-1/2 h-1/2 md:mt-0 md:h-93v 3xl:h-95v  object-cover"
-                    src={homeImage}
-                    alt=""
-                />
-            
+            <img
+                className="w-full md:w-1/2 h-1/2 md:mt-0 md:h-93v 3xl:h-95v  object-cover"
+                src={homeImage}
+                alt=""
+            />
         </main>
     );
 };
