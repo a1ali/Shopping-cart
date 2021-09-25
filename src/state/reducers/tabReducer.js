8fef856ccb34f4da21fe8d177ecd154a73caa7ee
Afaq Ali <afaqali2010@gmail.com>
2021-09-24 23:08:54 -0400

    cleanup

--- src/state/reducers/tabReducer.js
+++ src/state/reducers/tabReducer.js
@@ -1,14 +1,4 @@
 const tabReducer = (state = "home", action) => {
-    // switch (action.type) {
-    //     case "home":
-    //         return "home";
-    //     case "shop":
-    //         return "shop";
-    //     case "contact":
-    //         return "contact";
-    //     default:
-    //         return "home";
-    // }
     switch (action.type) {
         case "CHANGE_TAB":
             return action.payload
